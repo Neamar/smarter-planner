1c33f280d2b1bc7a869ba186bb365e43980a1fb5
Neamar <neamar@neamar.fr>
2021-11-21 19:09:23 +0100

    Super simple constraint system

---
+++ constraints.py
@@ -0,0 +1,38 @@
+def constraint_not(c):
+    def i(p):
+        print("not", c(p))
+        return not c(p)
+
+    return i
+
+
+def constraint_or(c1, c2):
+    def i(p):
+        print("or", c1(p), c2(p))
+        return c1(p) or c2(p)
+
+    return i
+
+
+def start_before(hour):
+    def i(p):
+        print("start before", hour, p.start.hour)
+        return p.start.hour < hour
+
+    return i
+
+
+def end_after(hour):
+    def i(p):
+        print("end after", hour, p.start.hour)
+        return p.start.hour >= hour
+
+    return i
+
+
+def weekend():
+    def i(p):
+        weekday = p.start.weekday()
+        return weekday == 5 or weekday == 6
+
+    return i
--- test.py
+++ test.py
@@ -2,6 +2,8 @@ from datetime import datetime, timedelta
 
 from tabulate import tabulate
 
+from constraints import *
+
 
 class Planning:
     name = ""
@@ -11,12 +13,15 @@ class Planning:
     period_length: timedelta
     periods = []
 
-    def __init__(self, name, location, start, end, period_length=timedelta(hours=1)):
+    resources = []
+
+    def __init__(self, name, location, resources, start, end, period_length=timedelta(hours=1)):
         self.name = name
         self.location = location
         self.start = start
         self.end = end
         self.period_length = period_length
+        self.resources = resources
         cursor = start
         while cursor < end:
             self.periods.append(Period(planning=self, start=cursor))
@@ -30,10 +35,15 @@ class Planning:
             today = []
             for period in self.periods:
                 if period.start.date() == day:
-                    today.append(period.assignment if period.assignment is not None else "?")
+                    today.append(period.assignment.name if period.assignment is not None else "?")
             out.append(today)
-        print(out)
         return tabulate(zip(hours, *out), headers=["hours", *days])
+
+    def fill(self):
+        for period in self.periods:
+            for resource in self.resources:
+                if resource.can_work(period):
+                    period.assignment = resource
 
 
 class Period:
@@ -54,17 +64,50 @@ class Resource:
     must = []
     should = []
 
-    def can_work(period: Period):
-        pass
+    def can_work(self, period):
+        for m in self.must:
+            if not m(period):
+                return False
+        return True
 
-    def __repr__(self):
+    def __str__(self):
         return self.name
 
 
+class NonWorkingHours(Resource):
+    name = "----"
+    must = [
+        constraint_or(constraint_not(end_after(8)), constraint_not(start_before(18))),
+    ]
+
+
+class Weekends(Resource):
+    name = "----"
+    must = [weekend()]
+
+
 class Employee(Resource):
-    must = []
+    name = "Employee"
+    must = [
+        constraint_not(start_before(8)),
+        constraint_not(end_after(18)),
+        constraint_not(weekend()),
+    ]
     should = []
 
 
-p = Planning("Librairie", "procure", datetime(2021, 11, 1), datetime(2021, 11, 8))
+class Prisca(Employee):
+    name = "Prisca"
+    pass
+
+
+p = Planning(
+    "Librairie",
+    "procure",
+    [NonWorkingHours(), Weekends(), Prisca()],
+    datetime(2021, 11, 1),
+    datetime(2021, 11, 8),
+)
+print(NonWorkingHours().can_work(Period(p, datetime(2021, 11, 1, 10))))
+p.fill()
 print(p)
